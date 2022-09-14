fee9411d0a10aad0b675011974404d6404a01578
Rafał Rybicki <rafarybicki@gmail.com>
2022-09-14 09:32:18 +0200

    .

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,49 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, except: %i[index new create]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body, :status)
+  end
+end
